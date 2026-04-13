6466a84bcb504e1760f0af3b3545ce830ab726f5
carlosRCA81 <carlos80sistema@gmail.com>
2026-04-13 16:17:41 -0400

    Actualizar security.js

--- security.js
+++ security.js
@@ -1,25 +1,18 @@
 const CLAVE_MAESTRA = '7575'; 
 
 function checkAccess() {
-    const inputPass = document.getElementById('access-key').value;
-    const loginScreen = document.getElementById('login-screen');
-    const mainApp = document.getElementById('main-app');
-
-    if (inputPass === CLAVE_MAESTRA) {
-        loginScreen.style.display = 'none';
-        mainApp.style.display = 'block';
-        
-        // Arranca la lógica de Ohio
-        if (typeof inicializarSistema === 'function') {
-            inicializarSistema();
-        }
+    const val = document.getElementById('access-key').value;
+    if (val === CLAVE_MAESTRA) {
+        document.getElementById('login-screen').style.display = 'none';
+        document.getElementById('main-app').style.display = 'block';
+        if (typeof inicializarSistema === 'function') inicializarSistema();
         
         setInterval(() => {
             const clock = document.getElementById('live-clock');
             if(clock) clock.innerText = new Date().toLocaleTimeString();
         }, 1000);
     } else {
-        alert("ERROR: CLAVE INCORRECTA");
+        alert("CLAVE ERRÓNEA");
     }
 }
 
